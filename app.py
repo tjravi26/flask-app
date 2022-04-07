8fc7924de014e6030dc542ebbddae1ec90020946
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-07 19:16:12 +0530

    Added few sql queries.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import psycopg2
@@ -16,36 +16,30 @@ DB_NAME = 'fake_api'
 DB_USERNAME = 'postgres'
 DB_PASSWORD = '7890'
 
-conn = None
-cur = None
+conn = psycopg2.connect(host=DB_HOST, dbname=DB_NAME,
+                        user=DB_USERNAME, password=DB_PASSWORD)
+cur = conn.cursor()
 
-try:
-    conn = psycopg2.connect(host=DB_HOST, dbname=DB_NAME,
-                            user=DB_USERNAME, password=DB_PASSWORD)
-    cur = conn.cursor()
-except Exception as error:
-    print(error)
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
 
+@app.route('/user/<id>')
+def user_posts(id):
+    cur.execute(f'SELECT title, body FROM posts WHERE id={id}')
+    user_posts = cur.fetchall()
+    return render_template('users_list.html', user_posts=user_posts)
+
+
+@app.rout('/post/<id>')
+def post_id(id):
+    cur.execute(f'SELECT "userId" FROM posts WHERE id={id}')
+    user_posts = cur.fetchall()
+    return render_template('users_list.html', user_posts=user_posts)
+
+
+@app.rout('/comment/<id>')
 #############
-
 db = SQLAlchemy(app)
 
 Migrate(app, db)
-
-
-####################
-
-
-@app.route('/post/<id>', methods='GET')
-def user_posts(id):
-
-    else:
-        return {"You are not allowed to post yet."}
 
 
 if __name__ == '__main__':
---
+++ users.py
@@ -0,0 +1,3 @@
+from app import Users
+
+all_users = Users.query.get(1)
