44265f94fcade60eb724a8b63f994718b222e644
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-07 17:49:11 +0530

    Added a cursor connection and error handling.

--- app.py
+++ app.py
@@ -8,16 +8,30 @@ app = Flask(__name__)
 
 # Database location setup
 
-DB_HOST = 'localhost'
-DB_NAME = 'fake_api'
-DB_USER = 'postgres'
-DB_PASS = '7890'
-
-conn = psycopg2.connect(host=DB_HOST, dbname=DB_NAME,
-                        user=DB_USER, password=DB_PASS)
-
 app.config['SECRET_KEY'] = 'potato'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+DB_HOST = 'localhost'
+DB_NAME = 'fake_api'
+DB_USERNAME = 'postgres'
+DB_PASSWORD = '7890'
+
+conn = None
+cur = None
+
+try:
+    conn = psycopg2.connect(host=DB_HOST, dbname=DB_NAME,
+                            user=DB_USERNAME, password=DB_PASSWORD)
+    cur = conn.cursor()
+except Exception as error:
+    print(error)
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
+
+#############
 
 db = SQLAlchemy(app)
 
@@ -25,66 +39,6 @@ Migrate(app, db)
 
 
 ####################
-
-
-# Model - AKA Table in database
-class Users(db.Model):
-
-    # MANUAL TABLE NAME CHOICE!
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    username = db.Column(db.String)
-    email = db.Column(db.String)
-    phone = db.Column(db.String(50))
-    website = db.Column(db.String)
-
-    def __init__(self, id, name, username,
-                 email, phone, website):
-        self.id = id
-        self.name = name
-        self.username = username
-        self.email = email
-        self.phone = phone
-        self.website = website
-    db.create_all()
-
-
-class Posts(db.Model):
-
-    __tablename__ = 'posts'
-
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer)
-    title = db.Column(db.String)
-    body = db.Column(db.String)
-
-    def __init__(self, id, userId, title, body):
-        self.id = id
-        self.userId = userId
-        self.title = title
-        self.body = body
-    db.create_all()
-
-
-class Comments(db.Model):
-
-    __tablename__ = 'comments'
-
-    id = db.Column(db.Integer, primary_key=True)
-    postId = db.Column(db.Integer)
-    name = db.Column(db.String)
-    email = db.Column(db.String)
-    body = db.Column(db.String)
-
-    def __init__(self, id, postId, name, email, body):
-        self.id = id
-        self.postId = postId
-        self.name = name
-        self.email = email
-        self.body = body
-    db.create_all()
 
 
 @app.route('/post/<id>', methods='GET')
