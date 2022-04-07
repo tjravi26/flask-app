36bb9a6a10e87cb1463c1239f3ea1fef5426892b
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-07 17:49:28 +0530

    Moved the database models to it.

--- setupdatabase.py
+++ setupdatabase.py
@@ -1,6 +1,66 @@
-from app import Posts, Users, Comments, db
+from app import db
 import json
 import requests
+
+
+# Model - AKA Table in database
+class Users(db.Model):
+
+    # MANUAL TABLE NAME CHOICE!
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    phone = db.Column(db.String(50))
+    website = db.Column(db.String)
+
+    def __init__(self, id, name, username,
+                 email, phone, website):
+        self.id = id
+        self.name = name
+        self.username = username
+        self.email = email
+        self.phone = phone
+        self.website = website
+    db.create_all()
+
+
+class Posts(db.Model):
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer)
+    title = db.Column(db.String)
+    body = db.Column(db.String)
+
+    def __init__(self, id, userId, title, body):
+        self.id = id
+        self.userId = userId
+        self.title = title
+        self.body = body
+    db.create_all()
+
+
+class Comments(db.Model):
+
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    postId = db.Column(db.Integer)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    body = db.Column(db.String)
+
+    def __init__(self, id, postId, name, email, body):
+        self.id = id
+        self.postId = postId
+        self.name = name
+        self.email = email
+        self.body = body
+    db.create_all()
 
 
 def json_api_posts():
